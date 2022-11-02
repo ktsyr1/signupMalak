bbc3e10d5b906c29196c7b049b07ed76b864b542
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2022-11-02 20:31:43 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Signup from "./signup/page";
+
+function App() {
+  return (
+    <div className="App">
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
